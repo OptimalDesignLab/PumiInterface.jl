8c30ee377e2425c2c8740e09e33e634ee4e8c86a
Jared Crean <jcrean01@gmail.com>
2016-06-16 10:45:12 -0400

    add more tests

--- test/pdepumiinterface3.jl
+++ test/pdepumiinterface3.jl
@@ -45,11 +45,11 @@ facts("----- Testing PdePumiInterface3DG -----") do
   @fact mesh.face_Nptr --> not(C_NULL)
   @fact mesh.el_Nptr --> not(C_NULL)
 
-  function checkNumbering(nshape_ptr, dim)
+  function checkNumbering(nshape_ptr, dim, cnt)
     for i=1:4
       nodes = countNodesOn(nshape_ptr, simplexTypes[i])
       if i == dim
-        @fact nodes --> 1
+        @fact nodes --> cnt
       else
         @fact nodes --> 0
       end
@@ -58,7 +58,7 @@ facts("----- Testing PdePumiInterface3DG -----") do
 
   for i=1:4
     numbering_shape = getNumberingShape(mesh.entity_Nptrs[i])
-    checkNumbering(numbering_shape, i)
+    checkNumbering(numbering_shape, i, 1)
   end
 
   @fact mesh.numVert --> 8
@@ -71,13 +71,47 @@ facts("----- Testing PdePumiInterface3DG -----") do
   @fact mesh.numEntitiesPerType --> [mesh.numVert, mesh.numEdge, mesh.numFace, mesh.numEl]
   @fact mesh.numTypePerElement --> [4, 6, 4, 1]
   @fact mesh.el_type --> apfTET
-  @fact mesh.face_type--> apfTRIANGLE
+  @fact mesh.face_type --> apfTRIANGLE
+  @fact mesh.isDG --> true
   @fact mesh.dim --> 3
   
   
   for i=1:min(length(mesh.faces), length(mesh.edges))
     @fact mesh.edges[i] --> not(mesh.faces[i])
   end
+
+  nodeshape = getNumberingShape(mesh.nodenums_Nptr)
+  checkNumbering(nodeshape, 4, 4)
+  dofshape = getNumberingShape(mesh.dofnums_Nptr)
+  checkNumbering(dofshape, 4, 4)
+
+  @fact mesh.bndry_offsets[1] --> 1
+  @fact mesh.bndry_offsets[2] --> 13
+
+  for i=1:mesh.numEl
+    for j=1:mesh.numNodesPerElement
+      for k=1:mesh.dim
+        @fact mesh.coords[k, j, i] --> greater_than(0.99)
+        @fact mesh.coords[k, j, i] --> less_than(3.01)
+      end
+    end
+  end
+  dofs_sorted = reshape(mesh.dofs, mesh.numDof)
+  sort!(dofs_sorted)
+  @fact unique(dofs_sorted) --> dofs_sorted
+
+  @fact maximum(mesh.dofs) --> mesh.numEl*mesh.numNodesPerElement*mesh.numDofPerNode
+  @fact minimum(mesh.dofs) --> 1
+
+  for i=1:mesh.numEl
+    @fact mesh.sparsity_counts[1, i] --> greater_than(0)
+    @fact mesh.sparsity_counts[2, i] --> less_than(5)
+  end
+
+  @fact size(mesh.neighbor_colors, 1) --> 5
+  @fact size(mesh.neighbor_nums, 1) --> 5
+
+
 
 
 
