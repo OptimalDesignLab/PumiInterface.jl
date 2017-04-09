41ed6dd604805ae033c206351c7e958496233c68
Jared Crean <jcrean01@gmail.com>
2017-04-09 18:50:08 -0400

    change download.sh to use https rather than ssh to appease Travis

--- deps/download.sh
+++ deps/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/SCOREC/core.git core
-git clone git@github.com:SCOREC/pumi-meshes.git
+git clone https://github.com/SCOREC/pumi-meshes.git
 #wget https://scorec.rpi.edu/pumi/pumi_test_meshes.tar.gz
 #tar xfz ./pumi_test_meshes.tar.gz
