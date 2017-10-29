b2dfc5b14d2c1157f27127cf420e925db6e5784c
Jared Crean <jcrean01@gmail.com>
2017-10-29 18:31:17 -0400

    make SubMeshData a pointer

--- src/submesh_create.cc
+++ src/submesh_create.cc
@@ -4,114 +4,74 @@
 
 #include "submesh_create.h"
 
-struct _SubMeshData
+SubMeshData::SubMeshData(apf::Mesh* _m_old, apf::Numbering* _numberings[], int* _el_list, int numel)  // useful constructor
 {
-  _SubMeshData() {};  // default constructor
+  m_old = _m_old;
+  dim = m_old->getDimension();
+  
+  numberings.resize(dim);
+  for (int i = 0; i <= dim; ++i)
+    numberings[i] = _numberings[i];
 
-  // create some data structures needed to construct the new mesh
-  // this constructor also creates the new mesh object, but does not create
-  // any mesh entities
-  _SubMeshData(apf::Mesh* _m_old, apf::Numbering* _numberings[], int* _el_list, int numel)  // useful constructor
-  {
-    m_old = _m_old;
-    dim = m_old->getDimension();
-    
-    numberings.resize(dim);
-    for (int i = 0; i <= dim; ++i)
-      numberings[i] = _numberings[i];
+  // create new mesh
+  gmi_register_null();
+  gmi_model* g = gmi_load(".null");
+  bool ismatched = false; //TODO
+  m_new = apf::makeEmptyMdsMesh(g, dim, ismatched);
+  m_new->changeShape(m_old->getShape(), false);
 
-    // create new mesh
-    gmi_register_null();
-    gmi_model* g = gmi_load(".null");
-    bool ismatched = false; //TODO
-    m_new = apf::makeEmptyMdsMesh(g, dim, ismatched);
+  // convert the el_list to a 0-based std::vector
+  el_list.resize(numel);
+  for (int i = 0; i < numel; ++i)
+    el_list[i] = _el_list[i] - 1;
 
-    // convert the el_list to a 0-based std::vector
-    el_list.resize(numel);
-    for (int i = 0; i < numel; ++i)
-      el_list[i] = _el_list[i] - 1;
-
-    // turn el_list into a set of MeshEntity*
-    auto el_N = numberings[dim];
+  // turn el_list into a set of MeshEntity*
+  auto el_N = numberings[dim];
 //    el_set.reserve(numel);
-    el_entities.resize(numel);
-    int idx = 0; // current index in el_entities
-    apf::MeshIterator* it = m_old->begin(dim);
-    apf::MeshEntity* e;
-    while ( (e = m_old->iterate(it)) )
+  el_entities.resize(numel);
+  int idx = 0; // current index in el_entities
+  apf::MeshIterator* it = m_old->begin(dim);
+  apf::MeshEntity* e;
+  while ( (e = m_old->iterate(it)) )
+  {
+    int elnum = apf::getNumber(el_N, e, 0, 0);
+    // if this element is in el_list
+    if ( std::binary_search(el_list.begin(), el_list.end(), elnum) )
     {
-      int elnum = apf::getNumber(el_N, e, 0, 0);
-      // if this element is in el_list
-      if ( std::binary_search(el_list.begin(), el_list.end(), elnum) )
-      {
-        el_entities[idx] = e;
-        idx += 1;
-         
+      el_entities[idx] = e;
+      idx += 1;
+       
 //        el_set.insert(e);
-      }
     }
+  }
 
-    assert(idx == numel);  // all entities found
+  assert(idx == numel);  // all entities found
 
-    // fill verts, edges, faces, regions with NULL
+  // fill verts, edges, faces, regions with NULL
 
-    verts = std::vector<apf::MeshEntity*>(m_old->count(0), NULL);
-    edges = std::vector<apf::MeshEntity*>(m_old->count(1), NULL);
-    faces = std::vector<apf::MeshEntity*>(m_old->count(2), NULL);
-    regions = std::vector<apf::MeshEntity*>(m_old->count(3), NULL);
-    entities[0] = verts;
-    entities[1] = edges;
-    entities[2] = faces;
-    entities[3] = regions;  
+  verts = std::vector<apf::MeshEntity*>(m_old->count(0), NULL);
+  edges = std::vector<apf::MeshEntity*>(m_old->count(1), NULL);
+  faces = std::vector<apf::MeshEntity*>(m_old->count(2), NULL);
+  regions = std::vector<apf::MeshEntity*>(m_old->count(3), NULL);
+  entities[0] = verts;
+  entities[1] = edges;
+  entities[2] = faces;
+  entities[3] = regions;  
 
-  }  // constructor
+}  // constructor
 
-  // default destructor works here
+//-----------------------------------------------------------------------------
+// julia iterface
 
-  // data members
-  // everything here is initialized
-  apf::Mesh* m_old = NULL;
-  std::vector<apf::Numbering*> numberings;
-//  apf::Numbering* numberings[] = {NULL};
-  apf::Mesh2* m_new = NULL;
-  std::vector<int> el_list; // 0-based
-  int dim = 0;  // dimension of mesh
-
-  // map of number on the old mesh to the MeshEntity* on the new mesh
-  // length = number of entities on old mesh, unused values set to NULL
-  std::vector<apf::MeshEntity*> verts;
-  std::vector<apf::MeshEntity*> edges;
-  std::vector<apf::MeshEntity*> faces;
-  std::vector<apf::MeshEntity*> regions;
-
-  // array of the above for easy looping
-  std::vector<apf::MeshEntity*> entities[4];
-
-  // vector of element MeshEntity* on old mesh
-  std::vector<apf::MeshEntity*> el_entities;
-//  std::unordered_set<apf::MeshEntity*> el_set; // el_list with O(1) test for containment
-};
-typedef struct _SubMeshData SubMeshData;
-
-// vertify input data is valid
-void checkInput(SubMeshData sdata);
-void createVertices(SubMeshData sdata);
-void createEntities(SubMeshData sdata);
-// create a vertex on the new mesh given a vertex on the old mesh
-// uses same geometry classification as original mesh
-apf::MeshEntity* createVert(SubMeshData sdata, apf::MeshEntity* vert);
-
-// create an entity from its one-level downward adjacencies
-apf::MeshEntity* createEntity(SubMeshData sdata, apf::MeshEntity* entity);
 
 // el_list is the 1-based list of elements to preserve (must be sorted in
 // ascending order)
 // numberings[dim] is the array of zero-based apf::Numbering objects that n
 // numbers the MeshEntities from dimension 0 to dim
-SubMeshData createSubMesh(apf::Mesh* m, apf::Numbering* numberings[],
+SubMeshData* createSubMesh(apf::Mesh* m, apf::Numbering* numberings[],
                           int* el_list, int numel)
 {
-  SubMeshData sdata = SubMeshData(m, numberings, el_list, numel);
+  SubMeshData* sdata = new SubMeshData(m, numberings, el_list, numel);
   createVertices(sdata);
   createEntities(sdata);
 
@@ -120,9 +80,22 @@ SubMeshData createSubMesh(apf::Mesh* m, apf::Numbering* numberings[],
   return sdata;
 }
 
+void writeNewMesh(SubMeshData* sdata, const char* fname)
+{
+  sdata->writeNewMesh(fname);
+}
+
+apf::Mesh2* getNewMesh(SubMeshData* sdata)
+{
+  return sdata->getNewMesh();
+}
+
+
+//-----------------------------------------------------------------------------
+// helper functions
 
 // check that this element list can be used with this mesh
-void checkInput(SubMeshData sdata)
+void checkInput(SubMeshData* sdata)
 {
   // if an assert below triggers, this may leak memory, but thats ok
 
@@ -132,8 +105,8 @@ void checkInput(SubMeshData sdata)
   // verify el_list is sorted
   bool failflag = false;
   int prev_val = -1;
-  for (std::vector<int>::iterator it = sdata.el_list.begin(); 
-                                  it != sdata.el_list.end(); ++it)
+  for (std::vector<int>::iterator it = sdata->el_list.begin(); 
+                                  it != sdata->el_list.end(); ++it)
   {
     failflag = failflag || (*it <= prev_val);
     prev_val = *it;
@@ -142,23 +115,23 @@ void checkInput(SubMeshData sdata)
       max_el = *it;
   }
 
-  assert(max_el < (int)sdata.m_old->count(sdata.dim));
+  assert(max_el < (int)sdata->m_old->count(sdata->dim));
   assert(!failflag);
 }  // checkInputs
 
-// create vertocies on the new mesh, populates sdata.verts
-void createVertices(SubMeshData sdata)
+// create vertocies on the new mesh, populates sdata->verts
+void createVertices(SubMeshData* sdata)
 {
   // Algorithm: loop over elements to be copied, see
   //            if vertex already created, if not create the entity
 
   apf::Downward down_verts;
-  apf::Mesh* m_old = sdata.m_old;
+  apf::Mesh* m_old = sdata->m_old;
   apf::MeshEntity* e;  // current vert
-  apf::Numbering* vert_N = sdata.numberings[0];
+  apf::Numbering* vert_N = sdata->numberings[0];
 
-  for (std::vector<apf::MeshEntity*>::iterator it = sdata.el_entities.begin();
-       it != sdata.el_entities.end(); ++it)
+  for (std::vector<apf::MeshEntity*>::iterator it = sdata->el_entities.begin();
+       it != sdata->el_entities.end(); ++it)
 
   {
     e = *it;
@@ -169,32 +142,32 @@ void createVertices(SubMeshData sdata)
     for (int i = 0; i < nverts; ++i)
     {
       int vertnum = apf::getNumber(vert_N, down_verts[i], 0, 0);
-      if ( sdata.verts[vertnum] == NULL) // vert not yet created
-        sdata.verts[vertnum] = createVert(sdata, down_verts[i]);
+      if ( sdata->verts[vertnum] == NULL) // vert not yet created
+        sdata->verts[vertnum] = createVert(sdata, down_verts[i]);
     }
   }  // loop over elements
 
 }  // createEntities
 
-void createEntities(SubMeshData sdata)
+void createEntities(SubMeshData* sdata)
 {
   // Algorithm: loop over elements, loop over dimensions 1 to dim,
   //            if entity not yet created, create it from lower adjacencies
 
   apf::Downward down_entities;
-  apf::Mesh* m_old = sdata.m_old;
-  auto dim = sdata.dim;
+  apf::Mesh* m_old = sdata->m_old;
+  auto dim = sdata->dim;
 
-  for (std::vector<apf::MeshEntity*>::iterator it = sdata.el_entities.begin();
-       it != sdata.el_entities.end(); it++)
+  for (std::vector<apf::MeshEntity*>::iterator it = sdata->el_entities.begin();
+       it != sdata->el_entities.end(); it++)
     for (int d = 1; d <= dim; ++d)
     {
       int nentities = m_old->getDownward(*it, d, down_entities);
       for (int i = 0; i < nentities; ++i)
       {
-        int entitynum = apf::getNumber(sdata.numberings[d], down_entities[i], 0, 0);
-        if (sdata.entities[d][entitynum] == NULL)
-          sdata.entities[d][entitynum] = createEntity(sdata, down_entities[i]);
+        int entitynum = apf::getNumber(sdata->numberings[d], down_entities[i], 0, 0);
+        if (sdata->entities[d][entitynum] == NULL)
+          sdata->entities[d][entitynum] = createEntity(sdata, down_entities[i]);
 
       }  // loop i
     } // loop d
@@ -204,59 +177,59 @@ void createEntities(SubMeshData sdata)
 
 // create a vertex on the new mesh given a vertex on the old mesh
 // uses same geometry classification as original mesh
-apf::MeshEntity* createVert(SubMeshData sdata, apf::MeshEntity* vert)
+apf::MeshEntity* createVert(SubMeshData* sdata, apf::MeshEntity* vert)
 {
 
   // get the old geometry
-  apf::ModelEntity* me = sdata.m_old->toModel(vert);
-  int me_type = sdata.m_old->getModelType(me);
-  int me_tag = sdata.m_old->getModelTag(me);
+  apf::ModelEntity* me = sdata->m_old->toModel(vert);
+  int me_type = sdata->m_old->getModelType(me);
+  int me_tag = sdata->m_old->getModelTag(me);
 
   // get/create the geometry on the new mesh
   // the new mesh was create with gmi_null geometry model, so it will create
   // geometric entities as needed
 
-  apf::ModelEntity* me_new = sdata.m_new->findModelEntity(me_type, me_tag);
-  apf::MeshEntity* vert_new = sdata.m_new->createVert(me_new);
+  apf::ModelEntity* me_new = sdata->m_new->findModelEntity(me_type, me_tag);
+  apf::MeshEntity* vert_new = sdata->m_new->createVert(me_new);
 
   // set coordinates
   apf::Vector3 coords;
-  sdata.m_old->getPoint(vert, 0, coords);
-  sdata.m_new->setPoint(vert, 0, coords);
+  sdata->m_old->getPoint(vert, 0, coords);
+  sdata->m_new->setPoint(vert, 0, coords);
 
   return vert_new;
 }  // createVert
 
 // create all remaining entities from lower adjacencies
-apf::MeshEntity* createEntity(SubMeshData sdata, apf::MeshEntity* entity)
+apf::MeshEntity* createEntity(SubMeshData* sdata, apf::MeshEntity* entity)
 {
   // get the old geometry
-  apf::ModelEntity* me = sdata.m_old->toModel(entity);
-  int me_type = sdata.m_old->getModelType(me);
-  int me_tag = sdata.m_old->getModelTag(me);
+  apf::ModelEntity* me = sdata->m_old->toModel(entity);
+  int me_type = sdata->m_old->getModelType(me);
+  int me_tag = sdata->m_old->getModelTag(me);
 
   // get/create the geometry on the new mesh
-  apf::ModelEntity* me_new = sdata.m_new->findModelEntity(me_type, me_tag);
+  apf::ModelEntity* me_new = sdata->m_new->findModelEntity(me_type, me_tag);
 
   // get the entity type and the lower adjacent entities that define it
-  auto e_type = sdata.m_old->getType(entity);
+  auto e_type = sdata->m_old->getType(entity);
   int dim = apf::Mesh::typeDimension[e_type];
   apf::Downward down_entities;
-  int nentities = sdata.m_old->getDownward(entity, dim-1, down_entities);
+  int nentities = sdata->m_old->getDownward(entity, dim-1, down_entities);
 
   // get the corresponding entities on the new mesh
   for (int i = 0; i < nentities; ++i)
   {
-    int e_num = apf::getNumber(sdata.numberings[dim-1], down_entities[i], 0, 0);
-    down_entities[i] = sdata.entities[dim-1][e_num];
+    int e_num = apf::getNumber(sdata->numberings[dim-1], down_entities[i], 0, 0);
+    down_entities[i] = sdata->entities[dim-1][e_num];
     assert(down_entities[i] != NULL); // entity already created
   }
 
   // create the entity
-  apf::MeshEntity* e_new = sdata.m_new->createEntity(e_type, me_new, down_entities);
+  apf::MeshEntity* e_new = sdata->m_new->createEntity(e_type, me_new, down_entities);
 
   // for curvilinear meshes, set the higher order node locations (if any)
-  apf::FieldShape* fshape = sdata.m_old->getShape();
+  apf::FieldShape* fshape = sdata->m_old->getShape();
 
   if (fshape->hasNodesIn(dim))
   {
@@ -264,8 +237,8 @@ apf::MeshEntity* createEntity(SubMeshData sdata, apf::MeshEntity* entity)
     apf::Vector3 coords;
     for (int i = 0; i < nnodes; ++i)
     {
-      sdata.m_old->getPoint(entity, i, coords);
-      sdata.m_new->setPoint(e_new, i, coords);
+      sdata->m_old->getPoint(entity, i, coords);
+      sdata->m_new->setPoint(e_new, i, coords);
     }
   }
 
--- src/submesh_create.h
+++ src/submesh_create.h
@@ -22,8 +22,71 @@
 //#include <math.h>
 //#include <string.h>
 
+class SubMeshData
+{
+  public:
+    SubMeshData() {};  // default constructor
+
+    // create some data structures needed to construct the new mesh
+    // this constructor also creates the new mesh object, but does not create
+    // any mesh entities
+    SubMeshData(apf::Mesh* _m_old, apf::Numbering* _numberings[], 
+                int* _el_list, int numel);  // useful constructor
+
+
+    // default destructor works here
+    
+    // data members
+    apf::Mesh* m_old = NULL;
+    std::vector<apf::Numbering*> numberings;
+  //  apf::Numbering* numberings[] = {NULL};
+    apf::Mesh2* m_new = NULL;
+    std::vector<int> el_list; // 0-based
+    int dim = 0;  // dimension of mesh
+
+    // map of number on the old mesh to the MeshEntity* on the new mesh
+    // length = number of entities on old mesh, unused values set to NULL
+    std::vector<apf::MeshEntity*> verts;
+    std::vector<apf::MeshEntity*> edges;
+    std::vector<apf::MeshEntity*> faces;
+    std::vector<apf::MeshEntity*> regions;
+
+    // array of the above for easy looping
+    std::vector<apf::MeshEntity*> entities[4];
+
+    // vector of element MeshEntity* on old mesh
+    std::vector<apf::MeshEntity*> el_entities;
+    //  std::unordered_set<apf::MeshEntity*> el_set; // el_list with O(1) test for containment
+   
+    // member functions
+    void writeNewMesh(const char* fname)
+    {
+      m_new->writeNative(fname);
+    }
+
+    apf::Mesh2* getNewMesh()
+    {
+      return m_new;
+    }
+};  // class SubMeshData
+
+// vertify input data is valid
+void checkInput(SubMeshData* sdata);
+void createVertices(SubMeshData* sdata);
+void createEntities(SubMeshData* sdata);
+// create a vertex on the new mesh given a vertex on the old mesh
+// uses same geometry classification as original mesh
+apf::MeshEntity* createVert(SubMeshData* sdata, apf::MeshEntity* vert);
+
+// create an entity from its one-level downward adjacencies
+apf::MeshEntity* createEntity(SubMeshData* sdata, apf::MeshEntity* entity);
+
+
+
 extern "C" {
-apf::Mesh2* createSubMesh(apf::Mesh* m, int* el_list, int numel);
+  apf::Mesh2* createSubMesh(apf::Mesh* m, int* el_list, int numel);
+  void writeNewMesh(SubMeshData* sdata, const char* fname);
+  apf::Mesh2* getNewMesh(SubMeshData* sdata);
 } // extern C
 
 #endif
