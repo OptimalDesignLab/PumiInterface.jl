0e425580ff2c36a68356d61f1e290ffc4c73ec99
Jared Crean <jcrean01@gmail.com>
2018-09-25 20:59:46 -0400

    update use_julialib_general.sh to not overwrite SCOREC_PREFIX

--- src/use_julialib_general.sh
+++ src/use_julialib_general.sh
@@ -3,6 +3,13 @@
 #module load pumi/core-sim
 export LD_LIBRARY_PATH=`pwd`/../install/lib:$LD_LIBRARY_PATH
 #export LD_LIBRARY_PATH=/usr/local/pumi/core-sim/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=`pwd`/../deps/core/build/install/lib:$LD_LIBRARY_PATH
-export PATH=`pwd`/../deps/core/build/install/bin:$PATH
-export SCOREC_PREFIX=`pwd`/../deps/core/build/install
+
+echo "SCOREX_PREFIX = $SCOREC_PREFIX"
+if [[ $SCOREC_PREFIX == "" ]]  # if someone else set SCOREC_PREFIX, they are
+                               # responsible for the bin and lib paths
+then
+  echo "setting SCOREC_PREFIX"
+  export LD_LIBRARY_PATH=`pwd`/../deps/core/build/install/lib:$LD_LIBRARY_PATH
+  export PATH=`pwd`/../deps/core/build/install/bin:$PATH
+  export SCOREC_PREFIX=`pwd`/../deps/core/build/install
+fi
