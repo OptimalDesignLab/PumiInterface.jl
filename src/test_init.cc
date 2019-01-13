1d91927950673408e6348cd375ad4272604a3b6a
Jared Crean <jcrean01@gmail.com>
2019-01-13 17:10:53 -0500

    cleanup test_init.cc

--- src/test_init.cc
+++ src/test_init.cc
@@ -11,199 +11,47 @@
 #include <apfNumbering.h>
 #include <apfShape.h>
 #include "mpi.h"
-#include "pumiInterface_config.h"
+//#include "pumiInterface_config.h"
+#define HAVE_SIMMETRIX
 #ifdef HAVE_SIMMETRIX
 #include <gmi_sim.h>
 #include <SimUtil.h>
 #endif
 
-//#include "dgSBPShape1.h"
-//#include "apfSBPShape.h"
 
-/*
-void printRemoteInfo(apf::Mesh* m)
+void printParams(apf::Mesh* m)
 {
 
-  int myrank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
-  // iostream
-  std::fstream fs;
-  char fname[50];
-  sprintf(fname, "output_%d.dat", myrank);
-  fs.open(fname, std::fstream::out);
-
-  // mesh data
-  apf::Sharing* shr = apf::getSharing(m);
-  apf::Copies copies;  // used with getRemotes
-  apf::CopyArray shares; // used with Sharing
-  int dim = m->getDimension();
-  apf::MeshIterator* it = m->begin(dim-1);
-  apf::MeshEntity* e;
-
-  apf::Parts parts;
-  apf::getPeers(m, dim-1, parts);
-
-  for (apf::Parts::iterator it = parts.begin(); it != parts.end(); ++it)
-  {
-    fs << "peer " << *it << std::endl;
-  }
-
-
-
-  int facenum = 0;
-  while ( (e = m->iterate(it)) )
-  {
-    fs << "\nfacenum = " << facenum << std::endl;
-
-    copies.clear();
-    m->getRemotes(e, copies);
-
-    fs << "printing remotes:" << std::endl;
-    for (apf::Copies::iterator it = copies.begin(); it != copies.end(); ++it)
-    {
-      fs << "part num = " << it->first << ", entity = " << it->second << std::endl;
-    }
-
-    fs << "printing shares:" << std::endl;
-    shares.setSize(0);
-    shr->getCopies(e, shares);
-
-    for (int i=0; i < int(shares.getSize()); ++i)
-    {
-      fs << "part num = " << shares[i].peer << ", entity = " << shares[i].entity << std::endl;
-    }
-
-  }
-
-  m->end(it);
-  fs.close();
-
-} // function printRemoteInfo
-*/
-
-void printModelEntities(gmi_model* g)
-{
-//  double p[2];
-//  double x[3];
-  double r[2];
-  for (int dim=0; dim <= 3; dim++)
-  {
-    std::cout << "dimension " << dim << " geometric entities" << std::endl;
-    gmi_iter* it = gmi_begin(g, dim);
-    gmi_ent* ge;
-    while ( (ge = gmi_next(g, it)) )
-    {
-      int ge_tag = gmi_tag(g, ge);
-      std::cout << "  geometry entity " << ge << " has tag " << ge_tag << std::endl;
-      if (dim == 1)
-      {
-        gmi_range(g, ge, 0, r);
-        std::cout << "    parametric range = " << r[0] << ", " << r[1] << std::endl;
-        bool isPeriodic = gmi_periodic(g, ge, 0);
-        std::cout << "    isPeriodic = " << isPeriodic << std::endl;
-      }
-    }
-    gmi_end(g, it);
-  }
-
-}
-
-
-void printMidpointErrors(apf::Mesh* m)
-{
-  gmi_model* g = m->getModel();
-
-  const int dim = 1;
-  apf::MeshIterator* it = m->begin(dim);
-  apf::MeshEntity* e;
-  gmi_ent* ge;
-  apf::Downward verts;
-  apf::Vector3 x1, x2;
-  double midpoint[3], newpoint[3], newp[2];
-  while ( (e = m->iterate(it)) )
-  {
-    ge = (gmi_ent*)m->toModel(e);
-    if (dim == 0)
-    {
-      m->getPoint(e, 0, x1);
-      x1.toArray(midpoint);
-    } else
-    {
-      m->getDownward(e, 0, verts);
-      m->getPoint(verts[0], 0, x1);
-      m->getPoint(verts[1], 0, x2);
-
-      midpoint[0] = (x1.x() + x2.x())/2;
-      midpoint[1] =( x1.y() + x2.y())/2;
-      midpoint[2] =( x1.z() + x2.z())/2;
-    }
-
-    for (int i=0; i < 3; ++i)
-      newpoint[i] = 0;
-
-    gmi_closest_point(g, ge, midpoint, newpoint, newp);
-
-    std::cout << "distance from midpoint to geometry is " << newpoint[0] - midpoint[0] << ", " << newpoint[1] - midpoint[1] << ", " << std::endl;
-  }
-
-  m->end(it);
-}
-
-
-void printModelClassification(apf::Mesh * m)
-{
-  apf::MeshEntity* e;
-  apf::ModelEntity* me;
-  int model_dim;
-  int model_tag;
-  int cnt = 0;
-  char const* entity_names[4] = { "vertex ", "edge ", "face ", "region "};
-
-  for (int i = 0; i < 4; ++ i)
-  {
-    apf::MeshIterator* it = m->begin(i);
-    cnt = 0;
-    while ((e = m->iterate(it)))
-    {
-      me = m->toModel(e);
-      model_dim = m->getModelType(me);
-      model_tag = m->getModelTag(me);
-      std::cout << entity_names[i] << cnt << " is classified on model entity ";
-      std::cout << model_tag << " of dimension " << model_dim << std::endl;
-      ++cnt;
-    }
-    std::cout << std::endl;
-  }
-        
-
-}  // end function
-
-
-void printCoordinates(apf::Mesh* m)
-{
-  apf::MeshEntity* e;
-  apf::Vector3 coords;
   apf::FieldShape* fshape = m->getShape();
-  apf::MeshIterator* it;
+  std::cout << "Printing CAD parametric coordinates for " << fshape->getName() << std::endl;
 
-  for (int dim=0; dim <= m->getDimension(); dim++)
+  apf::MeshEntity* e;
+  apf::MeshIterator* it;
+  apf::Vector3 p; p[2] = 0;
+  apf::ModelEntity* me;
+  for (int dim=0; dim <= m->getDimension(); ++dim)
   {
     if (!fshape->hasNodesIn(dim))
       continue;
 
-    std::cout << "Dimension " << dim << ":" << std::endl;
+    std::cout << "Dimension " << dim << std::endl;
     it = m->begin(dim);
-    int i = 0;
     while ( (e = m->iterate(it)) )
     {
-      m->getPoint(e, 0, coords);
-      std::cout << "  dimension " << dim << " entity " << i << " coords = " << coords.x() << ", " << coords.y() << ", " << coords.z() << std::endl;
-      
-    }
-  }
+      // assume 1 node per entity
+      m->getParam(e, p);
+      me = m->toModel(e);
+      int tag = m->getModelTag(me);
+      int me_dim = m->getModelType(me);
+      std::cout << "  entity " << e << " classified on model entity " << tag << " of dimension " << me_dim << " has parametric coords = " << p.x();
+      std::cout << ", " << p.y() << ", " << p.z() << std::endl;
 
-} // function printCoordinates
+    }
+    m->end(it);
+
+  }
+}
+
 
 int main (int argc, char** argv)
 {
@@ -254,17 +102,6 @@ int main (int argc, char** argv)
   // see if coordinates were moved into tags
   apf::MeshTag* coords_tag = m->findTag("coordinates_ver");
   std::cout << "coords_tag = " << coords_tag << std::endl;
-  apf::DynamicArray<apf::MeshTag*> tags;
-  m->getTags(tags);
-
-  for (unsigned int i=0; i < tags.getSize(); ++i)
-  {
-    std::cout << "tag " << i << " name = " << m->getTagName(tags[i]) << std::endl;
-  }
-
-//  std::cout << "before changing shape, coords = " << std::endl;
-//  printCoordinates(m);
-//  printRemoteInfo(m);
 
   // force the coordinates into the field
   if (coords_tag != 0)
@@ -274,16 +111,9 @@ int main (int argc, char** argv)
     apf::changeMeshShape(m, apf::getLagrange(order_orig), false);
   }
 
-  // print coordinates again
-  //std::cout << "after changing shape, coords = " << std::endl;
-  //printCoordinates(m);
-
-  std::cout << "printing model classification" << std::endl;
-  printModelClassification(m);
-  printModelEntities(g);
-  //printMidpointErrors(m);
-  apf::writeASCIIVtkFiles("output_check", m);
-//  apf::writeVtkFiles("output_check", m);
+  printParams(m);
+  //apf::writeASCIIVtkFiles("output_check", m);
+  apf::writeVtkFiles("output_check", m);
 
   std::cout << "finished writing paraview files" << std::endl;
 
