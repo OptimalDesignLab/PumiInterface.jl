7b66cb1e94e08064529e496aff076b2fd09005bc
Jared Crean <jcrean01@gmail.com>
2019-08-16 20:15:30 -0400

    fix SBP version

--- deps/build.jl
+++ deps/build.jl
@@ -23,7 +23,7 @@ global const MPI_URL = "https://github.com/JuliaParallel/MPI.jl.git"
 global const MPI_VER = "v0.5.0"
 
 global const SBP_URL = "https://github.com/OptimalDesignLab/SummationByParts.jl.git"
-global const SBP_VER = "v0.7"
+global const SBP_VER = "jc_v0.5"
 
 
 start_dir = pwd()
